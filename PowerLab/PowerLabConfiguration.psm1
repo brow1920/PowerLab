bfc538415ec5f3ce2099d9a61e305436e790c903
Adam Bertram <adbertram@gmail.com>
2015-09-20 20:53:25 -0500

    added some verbosity

--- PowerLab/PowerLabConfiguration.psm1
+++ PowerLab/PowerLabConfiguration.psm1
@@ -84,9 +84,11 @@ function New-PlDatabase
 				Write-Verbose -Message "Creating table [$($t.Name)] in database [$($Database)]"
 				$tbl = New-Object ('Microsoft.SqlServer.Management.Smo.Table') ($db, $t.Name, 'dbo')
 				$t.Columns.Column | foreach {
+					Write-Verbose -Message "Adding column [$($_.Name)] with type [$($typeConversions[$_.Type])]"
 					$col = new-object ('Microsoft.SqlServer.Management.Smo.Column') ($tbl, $_.Name, $typeConversions[$_.Type])
 					if ($_.PrimaryKey -eq 'Yes')
 					{
+						Write-Verbose -Message "The [$($_.Name)] column needs to be the primary key"
 						$col.Identity = $true
 						$col.IdentitySeed = 1
 						$col.IdentityIncrement = 1
@@ -103,10 +105,11 @@ function New-PlDatabase
 				$pk.IsClustered = $true
 				
 				$idxColName = ($t.Columns.Column | where { $_.Index -eq 'yes' }).Name
+				Write-Verbose -Message "The index column needs to be [$($idxColName)]"
 				$indexCol = new-object ('Microsoft.SqlServer.Management.Smo.IndexedColumn') ($pk, $idxColName)
 				$pk.IndexedColumns.Add($indexCol)
 				$tbl.Indexes.Add($pk)
-				
+
 				#Create the table
 				$tbl.Create()
 			}
