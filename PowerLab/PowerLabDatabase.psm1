9689f146ad9ea7a1b31f2f0ab7c15657d12a9d8c
Adam Bertram <adbertram@gmail.com>
2015-09-21 15:22:43 -0500

    Created PowerLabDatabase and moved functions over

---
+++ PowerLab/PowerLabDatabase.psm1
@@ -0,0 +1,122 @@
+function New-PlDatabase
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$Instance = (Get-PlDefaultDatabaseConfig).Instance.Name,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$Database = (Get-PlDefaultDatabaseConfig).Name,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[System.Xml.XmlElement[]]$Table = (Get-PlDefaultDatabaseConfig).Tables.Table
+		
+		
+	)
+	begin
+	{
+		$ErrorActionPreference = 'Stop'
+	}
+	process
+	{
+		try
+		{
+			if (Test-PlDatabase -Database $Database -Instance $Instance)
+			{
+				throw "The database [$($Database)] already exists"
+			}
+			$null = [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
+			$server = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList $Instance
+			$db = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Database($server, $Database)
+			$db.Create()
+			
+			$typeConversions = @{
+				'int' = [Microsoft.SqlServer.Management.Smo.Datatype]::Int
+				'varchar100' = [Microsoft.SqlServer.Management.Smo.Datatype]::NVarChar(100)
+				'datetime' = [Microsoft.SqlServer.Management.Smo.Datatype]::DateTime
+			}
+			
+			foreach ($t in $Table)
+			{
+				Write-Verbose -Message "Creating table [$($t.Name)] in database [$($Database)]"
+				$tbl = New-Object ('Microsoft.SqlServer.Management.Smo.Table') ($db, $t.Name, 'dbo')
+				$t.Columns.Column | foreach {
+					Write-Verbose -Message "Adding column [$($_.Name)] with type [$($typeConversions[$_.Type])]"
+					$col = new-object ('Microsoft.SqlServer.Management.Smo.Column') ($tbl, $_.Name, $typeConversions[$_.Type])
+					if ($_.PrimaryKey -eq 'Yes')
+					{
+						Write-Verbose -Message "The [$($_.Name)] column needs to be the primary key"
+						$col.Identity = $true
+						$col.IdentitySeed = 1
+						$col.IdentityIncrement = 1
+					}
+					else
+					{
+						$col.Nullable = $false
+					}
+					
+					$tbl.Columns.Add($col)
+				}
+				$pk = new-object ('Microsoft.SqlServer.Management.Smo.Index') ($tbl, "PK_$Database")
+				$pk.IndexKeyType = 'DriPrimaryKey'
+				$pk.IsClustered = $true
+				
+				$idxColName = ($t.Columns.Column | where { $_.Index -eq 'yes' }).Name
+				Write-Verbose -Message "The index column needs to be [$($idxColName)]"
+				$indexCol = new-object ('Microsoft.SqlServer.Management.Smo.IndexedColumn') ($pk, $idxColName)
+				$pk.IndexedColumns.Add($indexCol)
+				$tbl.Indexes.Add($pk)
+				
+				#Create the table
+				$tbl.Create()
+			}
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
+function Test-PlDatabase
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$Instance = (Get-PlConfigurationData).Configuration.Database.Instance.Name,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$Database = (Get-PlConfigurationData).Configuration.Database.Name
+	)
+	begin
+	{
+		$ErrorActionPreference = 'Stop'
+	}
+	process
+	{
+		try
+		{
+			$null = [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
+			$server = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList $Instance
+			if (-not $server.Databases[$Database])
+			{
+				$false
+			}
+			else
+			{
+				$true
+			}
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
